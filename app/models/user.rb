f0a9f6904e48586e5a296ba257acd91ac8e2784a
Derfler <rmderfler@gmail.com>
2014-08-31 18:44:33 -0700

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
 	has_secure_password
-	validates_uniqueness_of :email
+	validates_uniqueness_of :name
+	has_secure_password
+  validates_presence_of :name
+  has_many :photos
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
