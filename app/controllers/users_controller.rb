44d2535bf9e10e5ca2ed44a3e82653b82c2e3903
Derfler <rmderfler@gmail.com>
2014-08-31 18:47:14 -0700

    add user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,52 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "User was added"
+      redirect_to users_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      @user.update_attribute(:photo, params[:user][:photo])
+      flash[:notice] = "User updated."
+      redirect_to users_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      flash[:notice] = "The user was deleted."
+      redirect_to users_path
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:photo, :name, :password, :password_confirmation)
+    end
 end
