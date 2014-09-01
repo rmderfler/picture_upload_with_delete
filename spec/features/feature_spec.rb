5fe6d25c561ee34cd529a1c7367500a7d782930f
Derfler <rmderfler@gmail.com>
2014-09-01 00:01:38 -0700

    add logout test with capybara

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -4,7 +4,7 @@ describe "the signin process" do
   it "signs a user in who uses the right password" do
     visit '/login'
     user = User.create(:name => 'Joe', :password => 'password')
-    fill_in 'name', :with => 'Joe'
+    fill_in 'Name', :with => 'Joe'
     fill_in 'Password', :with => 'password'
     click_button 'Log in'
     expect(page).to have_content 'Logged in'
@@ -13,9 +13,34 @@ describe "the signin process" do
   it "gives a user an error who uses the wrong password" do
     visit '/sessions/new'
     user = User.create(:name => 'Joe', :password => 'password')
-    fill_in 'name', :with => 'Joe'
+    fill_in 'Name', :with => 'Joe'
     fill_in 'Password', :with => 'wrong'
     click_button 'Log in'
     expect(page).to have_content 'invalid'
   end
+
+  it "gives a user an error if the name is already taken" do
+    visit '/signup'
+    user = User.create(:name => 'T', :password => 'password')
+    another_user = User.create(:name => 'T', :password => 'password')
+    fill_in 'Name', :with => 'T'
+    fill_in 'Password', :with => 'password'
+    click_button 'Create User'
+    expect(page).to have_content 'Name has already been taken'
+  end
+
+
+describe "logout" do
+  it "logs out a signed in user" do
+    visit '/login'
+    user = User.create(:name => 'G', :password => 'password')
+    fill_in 'name', :with => 'G'
+    fill_in 'Password', :with => 'password'
+    click_button 'Log in'
+    visit '/'
+    click_on 'Log out'
+    expect(page).to have_content 'Logged out'
+  end
+end
+
 end
